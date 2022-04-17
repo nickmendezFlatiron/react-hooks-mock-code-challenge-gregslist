1f830b97eb51e8acec7880f0702eab00a4d86c0f
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-04-17 11:02:44 -0400

    Done.

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,39 @@
-import React from "react";
+import {React , useState} from "react";
 
-function ListingCard() {
+function ListingCard({listing, onDeleteListing}) {
+
+  const {id , description , image , location} = listing
+  const [toggle , setToggle] = useState(false)
+
+  function handleFavorite(){
+    setToggle(toggle => !toggle)
+  }
+
+  function handleDeleteListingClick(id){
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(() => onDeleteListing(listing))
+
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+
+        {toggle ? (
+          <button className="emoji-button favorite active" onClick={handleFavorite}>★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite" onClick={handleFavorite}>☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button className="emoji-button delete" onClick={() => handleDeleteListingClick(id)}>🗑</button>
       </div>
     </li>
   );
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import {React, useEffect, useState} from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [allListings , setAllListings] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:6001/listings')
+    .then(res => res.json())
+    .then(listings => {setAllListings(listings)
+    })
+  },[])
+
+  function handleDeleteListing(deletedListing){
+   const updatedListing = allListings.filter(listing => listing.id !== deletedListing.id)
+    setAllListings(updatedListing)
+  }
+
+  const renderListings = allListings.map(listing => <ListingCard listing={listing} onDeleteListing={handleDeleteListing}/>
+  )
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {renderListings}
       </ul>
     </main>
   );
